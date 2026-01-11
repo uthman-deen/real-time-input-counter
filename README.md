5e7c96e4bebd8fccc31e037ebb11154e557c4f0c
uthman-deen <deen.duniya.developer@gmail.com>
2026-01-11 11:26:41 +0100

    real time input counter

---
+++ README.md
@@ -0,0 +1,5 @@
+### Real Time Counter
+
+# It was basically built using HTML, CSS, JavaScript(vanilla)
+1. The real time counter counts the input in real time.
+2. It enforces the input length limit.
---
+++ script.js
@@ -0,0 +1,19 @@
+const textInput = document.querySelector("#text-input");
+const charCount = document.querySelector("#char-count");
+const maxLength = 50;
+
+textInput.addEventListener("input", () => {
+    if (textInput.value.length > maxLength) {
+        textInput.value = textInput.value.trim().slice(0, maxLength);
+    }
+
+    const length = textInput.value. length;
+
+    charCount.textContent = `Character Count: ${length}/${maxLength}`;
+
+    if (length === maxLength) {
+        charCount.style.color = "red";
+    } else {
+        charCount.style.color = "black";
+    }
+});
